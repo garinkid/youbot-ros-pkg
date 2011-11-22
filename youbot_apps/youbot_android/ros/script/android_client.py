65519f6abaa69b74acefcc5b5ccea873595f3b65
Rhama Dwiputra <garinkid@yahoo.com>
2011-11-22 13:32:38 +0100

    works wit cmd_vel

--- youbot_apps/youbot_android/ros/script/android_client.py
+++ youbot_apps/youbot_android/ros/script/android_client.py
@@ -2,17 +2,18 @@
 import roslib; roslib.load_manifest('youbot_android')
 import rospy
 import bluetooth
+import sys
+import threading
 from struct import unpack
 from geometry_msgs.msg import *
 
-
-class youbot_android():	
-	def __init__(self):
+class Bluetooth_Thread(threading.Thread):
+	def run(self):
 		self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 		self.port = 1
 		self.command = ''
 		self.data = []
-		
+
 		self.base_velocity = geometry_msgs.msg.Twist()
 		self.base_velocity.linear.x = 0.0
 		self.base_velocity.linear.y = 0.0
@@ -25,15 +26,13 @@ class youbot_android():
 		self.normalization_linear_y = 0.25
 		self.normalization_angular_z = 0.5
 		
-		self.base_velocity_publisher = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist)
-		# bind sock
 		self.server_sock.bind(("",self.port))
 		self.server_sock.listen(1)
 
 		print 'Waiting for connection...'
 		self.client_sock, self.address = self.server_sock.accept()
 		print "Accepted connection from", self.address
-		while True:
+		while not rospy.is_shutdown():
 			self.command = self.client_sock.recv(1024)
 			print "received %s" % self.command
 			if self.command == 'onPause':
@@ -53,10 +52,23 @@ class youbot_android():
 				self.base_velocity.linear.x = self.data[0] * self.normalization_linear_x
 				self.base_velocity.linear.y = self.data[1] * self.normalization_linear_y
 				self.base_velocity.angular.z = self.data[2] * self.normalization_angular_z
+
+
+class youbot_android():	
+	def __init__(self):
+		rospy.loginfo('youbot android node is running')
+		
+		bluetooth_thread = Bluetooth_Thread()
+		bluetooth_thread.setDaemon(True)
+		bluetooth_thread.start()
+
+		self.base_velocity = geometry_msgs.msg.Twist()
+		self.base_velocity_publisher = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist)
+		while not rospy.is_shutdown():
+			self.base_velocity = bluetooth_thread.base_velocity
 			self.base_velocity_publisher.publish(self.base_velocity)		
 
 if __name__ == '__main__':
 	rospy.init_node('youbot_android')
 	youbot_android()
-	rospy.loginfo('youbot android node is running')
 	rospy.spin()
