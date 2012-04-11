cc72983b7b10b99aeba4fb3e1e4332e460421d9e
Rhama Dwiputra <garinkid@yahoo.com>
2012-04-11 11:15:23 +0200

    first prototype

--- youbot_apps/youbot_android/ros/script/android_client.py
+++ youbot_apps/youbot_android/ros/script/android_client.py
@@ -5,6 +5,11 @@ import bluetooth
 import math
 import sys
 import threading
+pkg_dir = roslib.packages.get_pkg_dir('youbot_android')
+sys.path.append(pkg_dir + '/ros/src')
+sys.path.append(pkg_dir + '/ros/script')
+import low_pass_filter
+import pid_controller
 from brics_actuator.msg import *
 from geometry_msgs.msg import *
 from sensor_msgs.msg import *	
@@ -50,7 +55,7 @@ class Bluetooth_Thread(threading.Thread):
 		self.command_velocity.poisonStamp = poison_stamp
 		self.command_velocity.velocities = self.velocities
 
-		self.base_velocity = geometry_msgs.msg.Twist()
+
 
 		self.position_command = brics_actuator.msg.JointPositions()
 		self.unit_position = 'rad'# 's^-1 rad' #
@@ -78,7 +83,27 @@ class Bluetooth_Thread(threading.Thread):
 		self.joint_value_5_position.value = 0.0
 		self.positions = [self.joint_value_1_position, self.joint_value_2_position, self.joint_value_3_position, self.joint_value_4_position, self.joint_value_5_position]
 		self.position_command.poisonStamp = self.poison_stamp
-		self.position_command.positions = self.positions
+		self.position_command.positions = self.positions	
+
+
+		self.gripper_command = brics_actuator.msg.JointPositions()
+		self.unit_gripper = 'm'
+		self.gripper_l = brics_actuator.msg.JointValue()
+		self.gripper_l.joint_uri = 'gripper_finger_joint_l'
+		self.gripper_l.unit = self.unit_gripper 
+		self.gripper_l.value = 0.0
+		self.gripper_r = brics_actuator.msg.JointValue()
+		self.gripper_r .joint_uri = 'gripper_finger_joint_r'
+		self.gripper_r .unit = self.unit_gripper 
+		self.gripper_r .value = 0.0
+		self.gripper_positions = [self.gripper_l, self.gripper_r ]
+		self.gripper_command.poisonStamp = self.poison_stamp
+		self.gripper_command.positions = self.gripper_positions	
+		
+		self.base_velocity = geometry_msgs.msg.Twist()
+
+		self.low_pass_filters = [low_pass_filter.moving_average(60)] * 2
+		self.theta = [0.0] * 3
 
 		self.data_format = True
 
@@ -88,6 +113,8 @@ class Bluetooth_Thread(threading.Thread):
 		while not rospy.is_shutdown():
 			self.command = self.client_sock.recv(1024)
 			if self.command == 'onPause':
+				# self.low_pass_filters = [low_pass_filter.moving_average(60)] * 2
+				self.delta_theta = [0.0] * 2
 				self.client_sock.close()
 				self.server_sock.close()
 				self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -97,6 +124,7 @@ class Bluetooth_Thread(threading.Thread):
 				self.client_sock, self.address = self.server_sock.accept()
 				print "Accepted connection from", self.address
 			else:
+				#print self.data
 				self.data = self.command.split(',')
 				self.data_format = True
 				for i in range((len(self.data)) - 1):
@@ -111,6 +139,29 @@ class Bluetooth_Thread(threading.Thread):
 						self.android_manipulator(self.data)
 					elif self.data[0] == 'arm_joint_position': 
 						self.android_arm_joint_position(self.data)	
+					elif self.data[0] == 'gripper':
+						self.android_gripper(self.data)
+						self.data = []
+					elif self.data[0] == 'maze':
+						self.android_maze(self.data)
+								
+	
+	def android_maze(self, data):
+		if len(data) == 2:
+			if float(data[1]) == 1.0:
+				print "Maze game started"
+			elif float(data[1]) == 0.0:
+				print "Maze game stopped"
+			return
+		self.theta[0] = float(self.data[1])
+		self.theta[1] = float(self.data[2])
+		self.theta[2] = float(self.data[3])
+			
+	def android_gripper(self, data):
+		self.gripper_r .value = float(data[1])
+		self.gripper_positions = [self.gripper_l, self.gripper_r ]
+		self.gripper_command.positions = self.gripper_positions
+		print 'gripper set to -->', self.gripper_r .value, 'm'
 		
 	def android_base(self, data):
 		for i in range(3):
@@ -183,9 +234,11 @@ class youbot_android():
 		self.base_velocity = geometry_msgs.msg.Twist()
 		self.joint_velocity =  brics_actuator.msg.JointVelocities()
 		self.joint_position = brics_actuator.msg.JointPositions()
+		self.gripper = brics_actuator.msg.JointPositions()
 		self.base_velocity_publisher = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist)
 		self.joint_velocity_publisher = rospy.Publisher('/arm_1/arm_controller/velocity_command', brics_actuator.msg.JointVelocities)
 		self.joint_position_publisher = rospy.Publisher('/arm_1/arm_controller/position_command', brics_actuator.msg.JointPositions)	
+		self.gripper_publisher = rospy.Publisher('/arm_1/gripper_controller/position_command', brics_actuator.msg.JointPositions)
 
 		self.min_joint_limit = [False] * 5
 		self.max_joint_limit = [False] * 5
@@ -200,8 +253,11 @@ class youbot_android():
 			rospy.Subscriber('/joint_states', sensor_msgs.msg.JointState, self.state_callback)
 
 		self.base_velocity = bluetooth_thread.base_velocity	
-		self.joint_position = bluetooth_thread.position_command
 		self.joint_velocity = bluetooth_thread.command_velocity
+		self.joint_position = bluetooth_thread.position_command
+		self.gripper = bluetooth_thread.gripper_command
+		self.current_position = [0.0] * 5
+		# self.maze_velocity_controller = pid_controller.pd_controller(30)
 		while not rospy.is_shutdown():
 			if len(bluetooth_thread.data) > 0:
 				if bluetooth_thread.data[0] == 'base':
@@ -220,6 +276,28 @@ class youbot_android():
 							self.joint_velocity.velocities[i].value = 0.0
 					self.joint_velocity = bluetooth_thread.command_velocity
 					self.joint_velocity_publisher.publish(self.joint_velocity)		
+				elif bluetooth_thread.data[0] == 'gripper':
+					self.gripper = bluetooth_thread.gripper_command
+					self.gripper_publisher.publish(self.gripper)
+				elif bluetooth_thread.data[0] == 'maze':
+					theta = bluetooth_thread.theta
+					velocity = self.velocity_function(theta)
+					self.joint_velocity.velocities[4].value = velocity[0]
+					self.joint_velocity.velocities[3].value = velocity[1]
+					self.joint_velocity.velocities[2].value = velocity[2]
+					self.joint_velocity.velocities[1].value = velocity[3]
+					'''					
+					for i in range (1,4):
+						if (self.min_joint_limit[i] is True) and (velocity < 0):
+							self.joint_velocity.velocities[i].value = 0.0
+							print "joint ", i, " reach min limit"
+						elif (self.max_joint_limit[i] is True) and (velocity > 0.0):
+							self.joint_velocity.velocities[i].value = 0.0
+							print "joint ", i, " reach max limit"
+					'''
+					self.joint_velocity.velocities[0].value = 0.0
+					self.joint_velocity_publisher.publish(self.joint_velocity)	
+
 
 	def state_callback(self, msg):
 		# actual_limit
@@ -229,26 +307,44 @@ class youbot_android():
 		for i in range(5):
 			min_range[i] = min_range[i] + self.soft_stop_threshold
 			max_range[i] = max_range[i] - self.soft_stop_threshold 
-		current_position = [0.0] * 5
 
+		# position
 		for i in range(5):
 			if self.simulation is True:
-				current_position[i] = msg.position[i+8]
+				self.current_position[i] = msg.position[i+8]
 			else:
-				current_position[i] = msg.position[i]
+				self.current_position[i] = msg.position[i]
 
+		# check joint limit position
 		for i in range(5):
-			if current_position[i] < min_range[i]:
+			if self.current_position[i] < min_range[i]:
 				self.min_joint_limit[i] = True
 				self.max_joint_limit[i] = False
-			elif current_position[i] > max_range[i]:
+			elif self.current_position[i] > max_range[i]:
 				self.max_joint_limit[i] = True
 				self.min_joint_limit[i] = False
 			else:
 				self.min_joint_limit[i] = False
-				self.max_joint_limit[i] = False				
+				self.max_joint_limit[i] = False
+				
+	def velocity_function(self, theta):
+		game_sensitivity = theta[2]
+		velocity_set = [0.0] * 4
+		default_arm_pose_maze = [2.9172909934892601, 0.56619152215177582, -1.231205868905108, 2.5834732319552178, 2.9052652400289047]
+		delta_thetaX = math.radians(theta[0]) - (self.current_position[4] - default_arm_pose_maze[4])
+		delta_thetaY = math.radians(theta[1]) - (self.current_position[3] - default_arm_pose_maze[3])
+		# delta_thetaY =  
+		velocity_set[0] = delta_thetaX * 1.5 * game_sensitivity
+		velocity_set[1] = delta_thetaY * 1.0 * game_sensitivity
+		velocity_set[2] = delta_thetaY * -1.2 * game_sensitivity
+		velocity_set[3] = delta_thetaY * 0.8 * game_sensitivity
+		#print 'delta_theta:', delta_theta, ', velocity:', velocity_set
+		#velocity_value = self.maze_velocity_controller.control(velocity_set, self.current_velocity)
+		return velocity_set
 
 if __name__ == '__main__':
 	rospy.init_node('youbot_android')
 	youbot_android()
 	rospy.spin()
+
+## default arm position 2.9172909934892601, 0.56619152215177582, -1.231205868905108, 2.5834732319552178, 2.9052652400289047, 0.0, 0.0
